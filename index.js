52d5f19b631f306e0770e99459b36131c5709768
Michael Len <me@michaellen.com>
2013-08-08 01:33:31 -0700

    blog post adding functionality.  productionized index.js to not have debug code

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 
 var blog = require('./lib/blog');
-console.log (blog.api);
-var api = blog.api({
-        host     : 'localhost',
-        user     : 'root',
-        database : 'lensite' });
 
-var callback = function(err, posts) { console.log ("in callback! err: " + err + "posts: " + JSON.stringify(posts)); };
-
-api.posts(
-{
-  initial: 1,
-  count: 2,
-  direction: "newer"
-}, callback);
-
+module.exports = blog;
